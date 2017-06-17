56ee7b9de60abec62c2896819a1420ebdd849a81
Sheng Di <SDi@walmartlabs.com>
2017-06-16 17:28:51 -0700

    temporarily add tempo core contents

--- src/client/components/thoughts.jsx
+++ src/client/components/thoughts.jsx
@@ -22,17 +22,16 @@ const Thoughts = () => {
             original form, accompanied by English versions from the 1914 translation by H. Rackham.
           </p>
 
-          <h4 className={thoughtsStyles.contentTitle}>Case Studies</h4>
+          <h4 className={thoughtsStyles.contentTitle}>Tempo Core</h4>
           <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-            literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-            ollege in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-            and going through the cites of the word in classical literature, discovered the undoubtable source.
-            Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil)
-            by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-            The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            <a target="_blank" href="https://gecgithub01.walmart.com/electrode/tempo-core">tempo-core </a> version 8 release is a major refactor that fixes many hidden problems.
+            <br />Key wins are:
+            <br />
+              － A reduction of 100K in your app's JS bundle<br />
+            － Properly refreshed quimby fallback static data<br />
+          － No more huge resource draining but useless file system quimby cache
           </p>
-          
+
           <h4 className={thoughtsStyles.contentTitle}>Feeback</h4>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
