2099c0164e1ce2517799f3fa2cf03e5c78aed3e6
Sheng Di <SDi@walmartlabs.com>
2017-06-19 17:00:45 -0700

    update feedback

--- src/client/components/thoughts.jsx
+++ src/client/components/thoughts.jsx
@@ -33,7 +33,12 @@ const Thoughts = () => {
             so we spent literally no time on create applications boilerplate.
             And a large library of components came as a very nice bonus.
           </p>
-          <p className={thoughtsStyles.feedbackAuthor}>--- Sergey Chebykin</p>
+          <p className={thoughtsStyles.feedbackAuthor}>--- Walmart Cart & Checkout</p>
+
+          <p>
+            I worked with electrode in the Sams on Pangaea initiative and it did exactly what any app platform should do - Let developers focus more on the business logic and take care of the rest of the app's infra. As a consumer, I didn't have to worry about bootstrapping a new app/component or make decisions on what coding standards to use etc. This is highly important for developer productivity and scales well across the org. Electrode also comes along with modules such as electrode-csrf which helps fix security issues across various apps merely by changing their config file to use it. The bundle analyzer is a cool nifty tool as well to help keep our bundle leaner. Keeping up with archetype version also helped me to be up to date with the company and share collective knowledge on how we work on different problems across projects. There are certainly areas that can be improved to make it even better, but I am sure it is just a matter of prioritization and I am excited about the direction in which they are headed to.
+          </p>
+          <p className={thoughtsStyles.feedbackAuthor}>--- Sams Club</p>
         </Col>
       </Row>
     </Grid>
